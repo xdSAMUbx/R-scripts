921efd8378a104bf5e0612153411dd76344d0cda
Samuel Calderon <samuelcalderon1202@hotmail.com>
2025-11-09 22:18:32 -0500

    punto2

--- Tareas/taller2.R
+++ Tareas/taller2.R
@@ -1,10 +1,21 @@
+library(MASS)
 library(tidyr)
 library(nortest)
 library(lmtest)
 library(olsrr)
+library(car)
+library(caret)
+library(psych)
+library(skedastic)
+library(rms)
+library(nlme)
 
 setwd("G:\\Otros ordenadores\\Mi PC\\Universidad\\8 Semestre\\Econometria\\R-scripts\\Tareas")
 
+
+#############################
+#         Primer Punto      #
+#############################
 data("airquality")
 
 df <- as.data.frame(airquality)
@@ -42,7 +53,6 @@ panel(df$Temp, df$Ozone,
 par(op)
 
 # 2) Modelo RLM
-
 summary(mod1 <- lm(Ozone ~ Solar.R + Wind + Temp, data = df, na.action = na.omit))
 resmod1 <- residuals(mod1)
 sum(resmod1)
@@ -52,4 +62,86 @@ lillie.test(resmod1)
 # Heterocedasticidad
 bptest(Ozone ~ Solar.R + Wind + Temp, data = df)
 # Autocorrelación
-dwtest(Ozone ~ Solar.R + Wind + Temp,data = df)
+dwtest(Ozone ~ Solar.R + Wind + Temp,data = df,alternative = "two.sided")
+durbinWatsonTest(mod1,10)
+acfmod1 <- acf(resmod1, main="Autocorrelación Residuales", ylab="Autocorrelación", xlab="Rezago")
+acfmod1
+# Multicolinealidad
+mod1vif <- vif(mod1)
+
+# Transformación boxcox
+bc <- MASS::boxcox(Ozone ~ Solar.R + Wind + Temp, data=df, lambda = seq(-2,2,length=100))
+L <- bc$x[which.max(bc$y)]
+
+df$OzT1 <- ifelse(df$Ozone == 0,ln(df$Ozone),
+       df$Ozone^(L-1)/(L * geometric.mean(df$Ozone)^(L-1)))
+
+df$OzT2 <- ifelse(df$Ozone == 0,ln(df$Ozone),
+                  (df$Ozone^L-1)/L)
+
+df$OzT3 <- ifelse(df$Ozone == 0,ln(df$Ozone),
+                  df$Ozone^L)
+
+summary(mod1T1 <- lm(OzT1~Solar.R + Wind + Temp,data = df))
+shapiro.test(residuals(mod1T1))
+bptest(mod1T1)
+acf(residuals(mod1T1))
+durbinWatsonTest(mod1T1,10)
+
+summary(mod1T2 <- lm(OzT2~Solar.R + Wind + Temp,data = df,na.action = na.omit))
+# Normalidad
+shapiro.test(residuals(mod1T2))
+lillie.test(residuals(mod1T2))
+# Heterocedasticidad
+bptest(mod1T2)
+# Autocorrelación
+acf(residuals(mod1T2), main = "Autocorrelación en los Residuos",
+    ylab = "Autocorrelación", xlab  = "Rezagos")
+dwtest(mod1T2,alternative="two.sided")
+durbinWatsonTest(mod1T2,10)
+# Multicolinealidad
+vif(mod1T2)
+
+summary(mod1T3 <- lm(OzT3~Solar.R + Wind + Temp,data = df))
+shapiro.test(residuals(mod1T3))
+bptest(mod1T3)
+dwtest(mod1T3)
+acf(residuals(mod1T3))
+durbinWatsonTest(mod1T3,10)
+plot(density(residuals(mod1T3)))
+
+summary(mod2T2 <- lm(OzT2~Solar.R + Wind + Temp+Month + Day, data = df,na.action = na.omit))
+# Normalidad
+shapiro.test(residuals(mod2T2))
+lillie.test(residuals(mod2T2))
+# Heterocedasticidad
+bptest(mod2T2)
+# Autocorrelación
+dwtest(mod2T2)
+durbinWatsonTest(mod2T2,10)
+fin <- acf(residuals(mod2T2),main="Autocorrelación de los Residuos", xlab = "Rezagos",
+    ylab = "Autocorrelación")
+# Multicolinealidad
+vif(mod2T2)
+
+############################
+#       Segundo Punto      #
+############################
+
+data2 <- "G:\\Otros ordenadores\\Mi PC\\Universidad\\8 Semestre\\Econometria\\Taller2\\data7-24.csv"
+df2 <- read.csv(data2)
+summary(mod2 <- lm(salepric ~ sqft + bedrms + baths + garage + age + city, data = df2))
+plot(df2$sqft,residuals(mod2))
+plot(residuals(mod2))
+bptest(mod2)
+white(mod2)
+df2$e2 <- residuals(mod2)^2
+summary(mod22 <- lm(e2 ~ sqft, data = df2))
+df2$sigma2_est <- pmax(fitted(mod22),1e-6)
+w <-  1/df2$sigma2_est
+w_2 <- 1/df2$sqft^2
+summary(mod23 <- lm(salepric ~ sqft + bedrms + baths + garage + age + city, 
+                    data = df2, weights = w))
+summary(mod24 <- gls(salepric ~ sqft + bedrms + baths + garage + age + city, 
+    data = df2, weights = varFixed(~ w_2)))
+bptest(mod24)
